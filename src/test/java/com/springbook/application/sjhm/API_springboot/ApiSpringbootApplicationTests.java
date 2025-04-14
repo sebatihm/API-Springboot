3bdc04ead63e62623af45c90a3e7b6f5750a6d26
Sebati HM <sebashermon003@gmail.com>
2025-04-14 14:54:10 -0600

    Inicio proyecto capitulo 2

---
+++ src/test/java/com/springbook/application/sjhm/API_springboot/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbook.application.sjhm.API_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
