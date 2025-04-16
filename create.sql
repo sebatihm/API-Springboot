dfbb3958afce07d8f20f217bcbf9eb340d8e6f42
Sebati HM <sebashermon003@gmail.com>
2025-04-16 12:47:44 -0600

    Capitulo 7 Por alguna extraña razon no me deja correr las migraciones, o es mas no se por que, tambien hubo incompatibilidades con postgre pero se pudo solucionar

---
+++ create.sql
@@ -0,0 +1,4 @@
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
