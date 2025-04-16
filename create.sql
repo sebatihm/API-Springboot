e453142dddfdf495dc0c87d269db86746946881d
Sebati HM <sebashermon003@gmail.com>
2025-04-16 14:52:45 -0600

    Capitulo 7: Ya funcionan las migraciones

--- create.sql
+++ create.sql
@@ -2,3 +2,5 @@ create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar
 create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
 create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
 create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
+create table copsboot_user (auth_server_id uuid, id uuid not null, email varchar(255), mobile_token varchar(255), primary key (id));
